1debf861aca468c5bd253880d1c0063514edd630
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-01-29 20:48:23 +0000

    chore/mix: add package configuration

--- mix.exs
+++ mix.exs
@@ -1,12 +1,14 @@
-defmodule SimpleAuth.MixProject do
+defmodule EasyAuth.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :simple_auth,
-      version: "0.1.0",
+      app: :easy_auth,
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -22,6 +24,20 @@ defmodule SimpleAuth.MixProject do
       {:confex, "~> 3.4.0"},
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
       {:plug, "~> 1.7.1"}
+    ]
+  end
+
+  defp description do
+    "A Plug for HTTP basic authentication that's also easy to configure"
+  end
+
+  defp package do
+    [
+      name: :easy_auth,
+      files: ~w(mix.exs lib .formatter.exs README.md LICENSE),
+      maintainers: ["Jacopo Cascioli"],
+      licenses: ["MPL 2.0"],
+      links: %{"GitHub" => "https://github.com/Vesuvium/easy_auth"}
     ]
   end
 end
